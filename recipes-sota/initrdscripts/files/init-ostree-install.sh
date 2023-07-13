ee7e8ea300978d7e78306b8be4b6d8b5a2190f5b
Robert Yang <liezhi.yang@windriver.com>
2023-07-13 15:43:06 +0800

    init-ostree-install.sh: Fix for removing old boot menu
    ^Boot.*\* ${INSTBR}$ -> ^Boot.*\* ${INSTBR}
    
    This can make it match lines like:
    Boot0001* wrlinux-image-std VenHw(99e275e7-75a0-4b37-a2e6-c5385e6c00cb)
    
    And also fix the indent.
    
    Signed-off-by: Robert Yang <liezhi.yang@windriver.com>

--- recipes-sota/initrdscripts/files/init-ostree-install.sh
+++ recipes-sota/initrdscripts/files/init-ostree-install.sh
@@ -1768,14 +1768,14 @@ fi
 if [ -d /sys/firmware/efi/efivars ] ;then
     if which efibootmgr >/dev/null 2>&1; then
         mount -t efivarfs efivarfs /sys/firmware/efi/efivars
-        for oldboonum in `efibootmgr | grep "^Boot.*\* ${INSTBR}$" |sed "s/Boot\(.*\)\* ${INSTBR}/\1/g"`; do
+        for oldboonum in `efibootmgr | grep "^Boot.*\* ${INSTBR}" |sed "s/Boot\(.*\)\* ${INSTBR}.*/\1/g"`; do
             efibootmgr -b ${oldboonum} -B
-        if [ "$EFIBOOT_FIRST" = "1" ]; then
-            efibootmgr -b 0 -B >/dev/null 2>&1 || true
-            efibootmgr -o 0 -b 0 -c -w -L "${INSTBR}" -d "${INSTDEV}" -p "${p1}" -l '\EFI\BOOT\bootx64.efi'
-        else
-            efibootmgr -c -w -L "${INSTBR}" -d "${INSTDEV}" -p "${p1}" -l '\EFI\BOOT\bootx64.efi'
-        fi
+            if [ "$EFIBOOT_FIRST" = "1" ]; then
+                efibootmgr -b 0 -B >/dev/null 2>&1 || true
+                efibootmgr -o 0 -b 0 -c -w -L "${INSTBR}" -d "${INSTDEV}" -p "${p1}" -l '\EFI\BOOT\bootx64.efi'
+            else
+                efibootmgr -c -w -L "${INSTBR}" -d "${INSTDEV}" -p "${p1}" -l '\EFI\BOOT\bootx64.efi'
+            fi
         done
         efibootmgr -c -w -L "${INSTBR}" -d "${INSTDEV}" -p "${p1}" -l '\EFI\BOOT\bootx64.efi'
         umount /sys/firmware/efi/efivars
